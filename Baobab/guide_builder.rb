a61a8a8f5af92af5d8e481d81e93f90af07940b7
jaypee <joelperezurcelay@gmail.com>
2018-07-09 19:42:33 +0200

    Add datasheet and printingtips section building logic

--- Baobab/guide_builder.rb
+++ Baobab/guide_builder.rb
@@ -6,7 +6,21 @@ strings_file = 'strings.aesv.csv'
 template_file= 'baobab_template.erb'
 output_file= 'latex-doc.tex'
 
-#array con los lenguajes disponibles
+datasheet_table_strings_file = 'datasheet_table_strings.aesv.csv'
+printingtips_table_strings_file = 'printingtips_table_strings.aesv.csv'
+data_and_tips_section_template_file= 'data_and_tips_section_template.erb'
+data_and_tips_section_output_file= 'data_and_tips_section.tex'
+
+datasheet_table_strings=CSV.read(datasheet_table_strings_file , col_sep: "~" , headers: false)
+printingtips_table_strings=CSV.read(printingtips_table_strings_file , col_sep: "~" , headers: false)
+
+template = Tilt::ERBTemplate.new(data_and_tips_section_template_file)
+
+File.open(data_and_tips_section_output_file , 'w') do |file|
+  file.write template.render(Object.new, datasheet:datasheet_table_strings, printingtips:printingtips_table_strings) 
+end
+
+
 strings=CSV.read(strings_file , col_sep: "~" , headers: false)
 strings.unshift("")
 template = Tilt::ERBTemplate.new(template_file)
