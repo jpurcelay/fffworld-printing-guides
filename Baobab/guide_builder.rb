72840e299e992ee0e95f708bcf6e2f4b76eb5190
jaypee <joelperezurcelay@gmail.com>
2018-07-11 17:53:37 +0200

    Baobab guide finished

--- Baobab/guide_builder.rb
+++ Baobab/guide_builder.rb
@@ -3,6 +3,7 @@ require 'tilt'
 require 'erb'
 
 strings_file = 'strings.aesv.csv'
+captions_strings_file = 'captions_strings.aesv.csv'
 template_file= 'baobab_template.erb'
 output_file= 'latex-doc.tex'
 
@@ -10,23 +11,36 @@ datasheet_table_strings_file = 'datasheet_table_strings.aesv.csv'
 printingtips_table_strings_file = 'printingtips_table_strings.aesv.csv'
 data_and_tips_section_template_file= 'data_and_tips_section_template.erb'
 data_and_tips_section_output_file= 'data_and_tips_section.tex'
+data_and_tips_section_strings_file= 'data_and_tips_section_strings.aesv.csv'
+review_request_section_strings_file= 'review_request_section_strings.aesv.csv'
+review_request_section_template_file= 'review_request_section_template.erb'
+review_request_section_output_file= 'review_request_section.tex'
+
 
 datasheet_table_strings=CSV.read(datasheet_table_strings_file , col_sep: "~" , headers: false)
 printingtips_table_strings=CSV.read(printingtips_table_strings_file , col_sep: "~" , headers: false)
+data_and_tips_section_strings=CSV.read(data_and_tips_section_strings_file, col_sep: "~" , headers: false)
+review_request_section_strings=CSV.read(review_request_section_strings_file, col_sep: "~" , headers: false)
 
 template = Tilt::ERBTemplate.new(data_and_tips_section_template_file)
 
 File.open(data_and_tips_section_output_file , 'w') do |file|
-  file.write template.render(Object.new, datasheet:datasheet_table_strings, printingtips:printingtips_table_strings) 
+  file.write template.render(Object.new, strings:data_and_tips_section_strings , datasheet:datasheet_table_strings, printingtips:printingtips_table_strings) 
+end
+
+template = Tilt::ERBTemplate.new(review_request_section_template_file)
+
+File.open(review_request_section_output_file , 'w') do |file|
+  file.write template.render(Object.new, strings:review_request_section_strings) 
 end
 
 
 strings=CSV.read(strings_file , col_sep: "~" , headers: false)
-strings.unshift("")
+captions_strings=CSV.read(captions_strings_file , col_sep: "~" , headers: false)
 template = Tilt::ERBTemplate.new(template_file)
 
 File.open(output_file , 'w') do |file|
-  file.write template.render(Object.new, strings:strings) 
+  file.write template.render(Object.new, strings:strings , captions_strings:captions_strings) 
 end
 
 system "pdflatex #{output_file}"
